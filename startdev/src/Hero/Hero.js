bf6eb6f49a9d097d1e7430ecbdde58c91d50f1a0
Summer <summer.flowers94@gmail.com>
2026-03-02 20:05:22 -0600

    X2
    X2

--- startdev/src/Hero/Hero.js
+++ startdev/src/Hero/Hero.js
@@ -26,7 +26,7 @@ function Hero({ setActiveSection }) {
                     >
                         Think to
                     </span>
-                    <span className="text-white"> ProdX</span>
+                    <span className="text-white"> ProdX2</span>
                 </h1>
                 <p className="text-xl sm:text-2xl text-gray-300 mb-8 max-w-2xl mx-auto">
                     Turn ideas into reality
